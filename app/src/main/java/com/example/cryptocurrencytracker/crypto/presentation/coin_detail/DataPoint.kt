9e72100b8881616a31a653ec4d2f2f3f2f0b9cd5
Aamir <aaa@aaas-MacBook-Pro.local>
2024-11-22 02:51:27 +0530

    plotting points

---
+++ app/src/main/java/com/example/cryptocurrencytracker/crypto/presentation/coin_detail/DataPoint.kt
@@ -0,0 +1,9 @@
+package com.example.cryptocurrencytracker.crypto.presentation.coin_detail
+
+import kotlinx.coroutines.flow.Flow
+
+data class DataPoint(
+    val x: Float,
+    val y: Float,
+    val xLabel: String //formatted date
+)
